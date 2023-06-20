f6446dc918877fde66ccddef8a2e87958d39c99b
Saki <sbasken@gmail.com>
2023-06-20 13:35:14 -0700

    Complete contact components and dynamic routes

--- course_files_final/02/solution/pages/contacts/[contactId].js
+++ course_files_final/02/solution/pages/contacts/[contactId].js
@@ -6,11 +6,10 @@ export default function Contact() {
     const router = useRouter()
     const { contactId } = router.query
     const contact = contacts.find(contact => contact.id === contactId);
-    const [first, last] = contact?.name.split(" ");
     return(
     <Layout>
         <a href="#" onClick={() => router.back()}>Back</a>
-        <h2>Name: {last.toUpperCase()}, {first}</h2>
+        <h2>Name: {contact.name}</h2>
         <p>ID: {contact.id}</p>
         <h3>Country: {contact.country}</h3>
     </Layout>)
--- course_files_final/02/solution/pages/contacts/index.js
+++ course_files_final/02/solution/pages/contacts/index.js
@@ -5,15 +5,16 @@ import contacts from "../api/contacts"
 export default function Contacts() {
     return(
     <Layout>
-    <ul>
-        {contacts.map(contact => {
-           return(
-            <li>
-                <Link href={`contacts/${contact.id}`}>
-                    <a>{contact.name}</a>
-                </Link>
-           </li>)
-        })}
-    </ul>
+        <ul>
+            {contacts.map(contact => {
+                return(
+                    <li>
+                        <Link href={`contacts/${contact.id}`}>
+                            <a>{contact.name}</a>
+                        </Link>
+                    </li>
+                )
+            })}
+        </ul>
     </Layout>)
 }
